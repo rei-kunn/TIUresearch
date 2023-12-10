e457444da3654eb3792ff95bb0eb1ae8e93839ba
Rei <rei@Reis-MacBook-Pro.local>
2023-12-10 19:53:31 +0900

    update main method ports

--- demo/app.py
+++ demo/app.py
@@ -169,5 +169,5 @@ def summarize():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    setup_nltk()
+    app.run(debug=True)
